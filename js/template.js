82c94759405fb8a225dea00bfef80a93056e86db
jooprima <jooprima@gmail.com>
2020-05-02 01:07:35 +0700

    template

---
+++ js/template.js
@@ -0,0 +1,9 @@
+var vm = new Vue({
+  el: "#vue_det",
+  data: {
+    firstname: "yohanes",
+    lastname: "prima",
+    htmlcontent: "<div>Vue JS Tempalate</div>",
+    imgsrc: "images/logo.png",
+  },
+});
